f027f406c646eb8241ea9ceddd934bb488718231
Josh Jones <ohblonddev@gmail.com>
2021-05-09 00:01:05 +0100

    State methodology used in result output message

--- src/ports/cli/structopt.rs
+++ src/ports/cli/structopt.rs
@@ -5,7 +5,7 @@ use crate::application::ApplicationService;
 pub(crate) fn run_cli(application_service: &ApplicationService) {
     let opts = CliOptions::from_args();
     let rate = application_service.calculate_switching_success_rate_for_iterations(opts.iterations);
-    println!("rate: {}", rate);
+    println!("switching success rate: {}", rate);
 }
 
 /// Demo of the switching solution to the Monty Hall problem
